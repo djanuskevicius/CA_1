dbce4c9177dea41bd6096d8004594d189cb2ec32
Ernestas Rutkauskas <info@mediaern.lt>
2023-04-21 20:48:55 +0300

    added changes based on PR comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const HomePage = lazy(() => import('./pages/HomePage'));
 const TodoListPage = lazy(() => import('./pages/TodoListPage'));
 
 function App() {
-  console.log('testing');
+  console.log('TESTING');
   const router = createBrowserRouter([
     {
       path: '/',
