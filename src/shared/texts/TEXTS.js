c344fe6e660a9c910fdfa2c34758e59cc0b99b70
djanuskevicius <106997462+djanuskevicius@users.noreply.github.com>
2023-04-25 19:37:54 +0300

    Update TEXTS.js
    Deleted trash.

--- src/shared/texts/TEXTS.js
+++ src/shared/texts/TEXTS.js
@@ -38,8 +38,6 @@ const TEXTS = {
           lt: 'Ši "TaskMate" programėlė yra patogus ir efektyvus įrankis visiems, norintiems nepamiršti savo dienotvarkės ir efektyviai valdyti savo kasdienes užduotis.',
           en: 'This "TaskMate" to-do list app is a convenient and efficient tool for anyone looking to stay organized and manage their daily tasks effectively.',
         },
-        // lt: 'Programėlė "TaskMate" yra paprasta ir intuityvi užduočių sąrašo programa, leidžianti vartotojams kurti ir valdyti sukurtas užduotis. Ji turi patogią sąsają, kuri leidžia naujiems ar esamiems vartotojams lengvai prisijungti ir pasiekti savo asmeninį darbų sąrašą. Prisijungę vartotojai gali peržiūrėti dabartinį užduočių sąrašą ir greitai pridėti naujas užduotis, atnaujinti esamas užduotis, lengvai pašalinti jas iš sąrašo, kai užduočių nebereikia. Ši "TaskMate" programėlė yra patogus ir efektyvus įrankis visiems, norintiems nepamiršti savo dienotvarkės ir efektyviai valdyti savo kasdienes užduotis.',
-        // en: 'The "TaskMate" app is a simple and intuitive to-do list application that allows users to create and manage tasks. It has a user-friendly interface that enables new or existing users to log in easily and access their personalized to-do list. Once logged in, users can view their current list of tasks and quickly add new ones, update their existing tasks, easily remove it from their list when not needed. This "TaskMate" to-do list app is a convenient and efficient tool for anyone looking to stay organized and manage their daily tasks effectively.',
       },
     },
     todoListPage: {
