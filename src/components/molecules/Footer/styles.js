b8492643d72229c2cc82c3486265d17a3d2283e5
djanuskevicius <106997462+djanuskevicius@users.noreply.github.com>
2023-04-25 20:02:31 +0300

    Update styles.js
    padding was missing a ":", added that.

--- src/components/molecules/Footer/styles.js
+++ src/components/molecules/Footer/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.div`
   width: 100%;
-  padding 30px;
+  padding: 30px;
   text-align: center;
   background-color: #a393eb;
 
